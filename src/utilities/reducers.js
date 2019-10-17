135e3616f4598577fed673103ea121910a48d234
Jurii Lubiankin <iurii.lubiankin@gmail.com>
2019-10-17 17:56:35 +0200

    Add initial state

--- src/utilities/reducers.js
+++ src/utilities/reducers.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
 import { SET_RATE } from './actions';
 
-function exchangeRateReducer(state = [], action) {
+const initialState = {
+  exchangeRate: 4.2789
+}
+
+
+
+function exchangeRateReducer(state = initialState, action) {
   switch (action.type) {
     case SET_RATE: 
       return [
