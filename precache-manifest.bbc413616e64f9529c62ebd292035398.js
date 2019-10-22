3bd3614e18c78545dea69207a9641f43aaf46504
Jurii Lubiankin <iurii.lubiankin@gmail.com>
2019-10-22 20:31:57 +0200

    Updates

---
+++ precache-manifest.bbc413616e64f9529c62ebd292035398.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74386f84dd6e5f75d700aee0ccc8445",
+    "url": "/currency-board/index.html"
+  },
+  {
+    "revision": "bf808a2efd94f29333c2",
+    "url": "/currency-board/static/css/main.a38e0a2a.chunk.css"
+  },
+  {
+    "revision": "7d186f702895d53b1589",
+    "url": "/currency-board/static/js/2.e2e98508.chunk.js"
+  },
+  {
+    "revision": "bf808a2efd94f29333c2",
+    "url": "/currency-board/static/js/main.3793c318.chunk.js"
+  },
+  {
+    "revision": "bb658e15c28b119b0b7f",
+    "url": "/currency-board/static/js/runtime-main.45764890.js"
+  }
+]);
